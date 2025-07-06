a36f52adcb130a6b724c053e49727f396f016ec8
upi <lutfinurrosyadi30@gmail.com>
2025-07-07 00:25:02 +0700

    Fix filament asset loading on Railway

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,14 @@ export default defineConfig({
             ],
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+        },
+    },
 })
